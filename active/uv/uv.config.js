6b6ae84fe6efb41c3b814a5d9e27b045409ea813
tf7software <160179762+tf7software@users.noreply.github.com>
2025-02-14 21:05:55 -0700

    Update uv.config.js

--- active/uv/uv.config.js
+++ active/uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: '/active/go/',
-    bare:'https://plexilearcade.xyz/bare/',
+    bare:'https://skibidi.global.ssl.fastly.net/bare/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/active/uv/uv.handler.js',
