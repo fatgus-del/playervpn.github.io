5cd537bac84048b84fe18b42be76ad8d83ac777a
Orbit. <159073116+1056516@users.noreply.github.com>
2025-02-19 10:25:38 -0500

    Update uv.config.js

--- active/uv/uv.config.js
+++ active/uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: '/active/go/',
-    bare:'https://forg.gaudencio.net.br/bare/',
+    bare:'https://englishwork4school.mka.in/bare/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/active/uv/uv.handler.js',
