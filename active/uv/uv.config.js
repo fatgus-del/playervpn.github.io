7f3b343f760aaf7462ee44b06926fd5d9d48ccf0
Orbit. <159073116+1056516@users.noreply.github.com>
2025-02-24 09:56:39 -0500

    Update uv.config.js

--- active/uv/uv.config.js
+++ active/uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: '/active/go/',
-    bare:'find.q-station.net/bare/',
+    bare:'https://ly.rchamberlinwoodworking.com/bare/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/active/uv/uv.handler.js',
