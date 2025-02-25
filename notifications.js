43c70c2dfa1be92771a17abb5d50455f2702a734
Orbit. <159073116+1056516@users.noreply.github.com>
2025-02-24 20:39:01 -0500

    Update notifications.js

--- notifications.js
+++ notifications.js
@@ -1,36 +1,59 @@
 function requestNotificationPermission() {
-  if (Notification.permission === 'default') {
-    Notification.requestPermission().then(permission => {
-      if (permission === 'granted') {
-        console.log("Permission granted for notifications.");
-      } else {
-        console.log("Permission denied for notifications.");
-      }
-    });
-  }
-}
-
-window.onload = function() {
-  requestNotificationPermission();
-  
-  document.addEventListener("visibilitychange", function() {
-    if (document.hidden) {
-      showNotification("PlayerVPN Games", "all the games you want...");
-    } else {
-      showNotification("PlayerVPN Games", "all the games you want...");
+    if (Notification.permission === 'default') {
+      Notification.requestPermission().then((permission) => {
+        if (permission === 'granted') {
+          console.log("Permission granted for notifications.");
+        } else {
+          console.log("Permission denied for notifications.");
+        }
+      });
     }
-  });
-};
-
-function showNotification(title, body) {
-  if (Notification.permission === 'granted') {
-    const notification = new Notification(title, {
-      body: body,
-      icon: 'https://i.ibb.co/N75vkX7/PVPN.png'
-    });
-
-    notification.onclick = function() {
-      window.focus();  
-    };
   }
-}
+  
+  function isUserOnWebsite() {
+    return window.location.href === "https://playervpn.github.io/";
+  }
+  
+  function showNotification(title, body) {
+    if (Notification.permission === 'granted') {
+      const notification = new Notification(title, {
+        body: body,
+        icon: 'https://i.ibb.co/N75vkX7/PVPN.png',
+      });
+  
+      notification.onclick = function () {
+        window.open('https://playervpn.github.io/', '_blank');
+      };
+    }
+  }
+  
+  function startInactivityTimer() {
+    let timer;
+  
+    function resetTimer() {
+      if (timer) {
+        clearTimeout(timer);
+      }
+
+      timer = setTimeout(() => {
+        if (!isUserOnWebsite()) {
+          showNotification(
+            "Woah, you've been gone a while!",
+            "Come play again, or even search, watch movies and shows, or talk to our AI!"
+          );
+        }
+      }, 1000);
+    }
+  
+    document.addEventListener('mousemove', resetTimer);
+    document.addEventListener('keypress', resetTimer);
+    document.addEventListener('scroll', resetTimer);
+  
+    resetTimer();
+  }
+
+  window.onload = function () {
+    requestNotificationPermission();
+    startInactivityTimer();
+  };
+  
