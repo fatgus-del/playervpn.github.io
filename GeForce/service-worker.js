efa49655f1ba7affe0ac34328af8f95672ca6738
Orbit. <159073116+1056516@users.noreply.github.com>
2025-03-07 23:06:34 -0500

    Create service-worker.js

---
+++ GeForce/service-worker.js
@@ -0,0 +1,22 @@
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open('v1').then((cache) => {
+      return cache.addAll([
+        '/',
+        '/GeForce/manifest.json',
+        '/GeForce/index.html',
+        '/GeForce/service-worker.js',
+        '/geforce.png',
+        '/geforce.png'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
